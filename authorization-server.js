065f108e45fbbd669acc4e48ffa7477bd448acde
bharadwajrishi <rishi.raj1616@gmail.com>
2021-04-18 11:04:12 +0800

    Added get and post requests
    1. Added get request + filtering the requests with restricted users and authentications.
    2. Added post request and authentication token request along with some token validations.

--- authorization-server.js
+++ authorization-server.js
@@ -2,6 +2,7 @@ const fs = require("fs")
 const express = require("express")
 const bodyParser = require("body-parser")
 const jwt = require("jsonwebtoken")
+const utils = require("utils")
 const {
 	randomString,
 	containsAll,
@@ -53,6 +54,81 @@ app.use(bodyParser.urlencoded({ extended: true }))
 /*
 Your code here
 */
+app.get('/authorize/:client_id', (req, res) => {
+	const client = clients.find(c => c == req.params.client_id);
+	if(client == 'undefined'){
+		return res.status(401).send();
+	}
+	const currentScopes = req.query.scope.split(" ")
+	const hasScope = utils.containsAll(client.scopes, currentScopes);
+	if(!hasScope) return res.statusCode(401).send();
+
+	const key = utils.randomString();
+	const value = req.query;
+
+	const requestId = [];
+	requestId.push({
+		key: value
+	});
+
+	var params = [];
+	params.push(
+		{ 'client': client  },
+		{ 'scope': req.query.scope },
+		{ 'requestId':  requestId });
+	res.render('login', )
+	res.status(200).send();
+	res.end();
+});
+
+app.post('approve', (req, res) => {
+	const currentUserName = req.body['username'];
+	const currentPassword = req.body['password'];
+
+	const currentUser = users.find(u => u == currentUserName);
+	if(!currentUser ||  users[currentUser] != currentPassword) 
+		return res.status(401).send();
+
+	const request = requests[req.body.requestId];
+	if(!request) return res.status(401).send();
+
+	const authCodeValue = {
+		'clientReq': request,
+		'userName': currentUserName,
+	};
+
+	const authCode = utils.randomString();
+	authorizationCodes.push({
+		authCode : authCodeValue
+	});
+
+	res.redirect(request.redirectUri +'?code=' + authCode + '&state=' + request.state);
+});
+
+app.post('/token', (req, res) => {
+	if(!req.headers.authorization) return res.status(401).send();
+	const decodedAuthCode = utils.decodeAuthCredentials(req.headers.authorization);
+
+	const client = clients.find(c => c == decodedAuthCode.clientId);
+	if(!client || client.clientSecret != decodedAuthCode.clientSecret)
+		return res.status(401).send();
+	
+	const obj = authorizationCodes[req.body.code];
+	if(obj == 'undefined'){
+		res.status(401).send();
+	}else{
+		delete authorizationCodes[req.body.code];
+	}
+
+	const userName = obj.userName;
+	const scope = obj.clientReq.scope;
+	var privateKey = fs.readFileSync('/assets/private_key.pem');
+	var token = jwt.sign({ userName : scope }, privateKey, { algorithm: 'RS256' });
+	res.status(201).json(
+		{ "access_token" : token },
+		{ "token_type" : 'Bearer' },
+	);
+});
 
 const server = app.listen(config.port, "localhost", function () {
 	var host = server.address().address
